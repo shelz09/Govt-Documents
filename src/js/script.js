47083fc8617e3fa2c74526d0ae299f848df739ac
Shlesh Choudhary <chaudharyshlesh1109@gmail.com>
2025-08-21 21:56:59 +0530

    updates

---
+++ src/js/script.js
@@ -0,0 +1,62 @@
+const login_gate = document.querySelector('.login-gate');
+const openAlert = document.getElementById("dashboard-btn");
+const closeAlert = document.querySelector(".alert-btn");
+const alertbox = document.querySelector(".alert");
+const lightmode = document.querySelector(".light-mode-btn");
+const nightmode = document.querySelector(".night-mode-btn");
+const navbar = document.querySelector(".navbar");
+
+
+
+const handleScroll = () => {
+    if(window.scrollY > 10){
+        navbar.style.backdropFilter = "blur(10px)";
+    }
+    else{
+        navbar.style.backgroundColor = "transparent"
+    }
+}
+
+
+window.addEventListener("scroll", handleScroll)
+
+
+// --- Apply saved theme on page load ---
+if (localStorage.getItem("theme") === "night") {
+    document.body.classList.add("night-mode");
+    lightmode.classList.remove("hide");
+    nightmode.classList.add("hide");
+} else {
+    document.body.classList.remove("night-mode");
+    lightmode.classList.add("hide");
+    nightmode.classList.remove("hide");
+}
+
+// --- Page redirection ---
+login_gate.addEventListener("click", () => {
+    window.location.href = "login.html";
+});
+
+// --- Alert toggle ---
+openAlert.addEventListener("click", () => {
+    alertbox.classList.toggle("hide");
+});
+closeAlert.addEventListener("click", () => {
+    alertbox.classList.add("hide");
+});
+
+// --- Night Mode On ---
+nightmode.addEventListener("click", () => {
+    document.body.classList.add("night-mode");
+    localStorage.setItem("theme", "night");
+    lightmode.classList.remove("hide");
+    nightmode.classList.add("hide");
+});
+
+// --- Light Mode On ---
+lightmode.addEventListener("click", () => {
+    document.body.classList.remove("night-mode");
+    localStorage.setItem("theme", "light");
+    lightmode.classList.add("hide");
+    nightmode.classList.remove("hide");
+});
